7c5170de683954ff2711d7eea29c08f308aa0c41
Alfredo Rojas <alfrs1293@gmail.com>
2019-04-10 20:21:42 -0600

    missing files

--- Modulo 1 - Analisis de Datos/Proyectos/Proyecto Final/testing.py
+++ Modulo 1 - Analisis de Datos/Proyectos/Proyecto Final/testing.py
@@ -1,25 +1,298 @@
-import pj_data_cleaning as p
 import pandas as pd
+import sqlite3 as lite
+
+const_path = r'C:\git\cenfotec\Modulo 2 - Gestion de Datos para la toma de decisiones\Proyecto\Datasets\IMDB Datasets'
+
+pd.set_option('display.max_rows', 1000)
+pd.set_option('display.max_columns', 10)
+pd.set_option('display.width', 1000)
+pd.options.mode.chained_assignment = None  # default='warn'
+
+def load_kaggle_data():
+    print("Loading Kaggle data")
+    titles_raw = pd.read_csv(
+        const_path+r"\imdb-5000-movie-dataset.zip"
+        , sep=','
+        , dtype={'startYear': object}
+        , compression='zip'
+    )
+    movie_titles = titles_raw  # [titles_raw['titleType'] == 'movie']
+    movie_titles['movie_imdb_link'] = 'tt' + movie_titles['movie_imdb_link'].map(
+        lambda x: x.lstrip(r'http://www.imdb.com/title').rstrip('?ref_=fn_tt_tt_1')
+    )
+    movie_titles['movie_imdb_link'] = movie_titles['movie_imdb_link'].map(
+        lambda x: x.rstrip('/')
+    )
+    movie_titles = movie_titles.rename(columns={'movie_imdb_link': 'tconst'})
+    print("Titles Kaggle loaded")
+    return movie_titles
+
+def load_title_data(kaggle_data):
+    print("Loading titles data")
+    titles_raw = pd.read_csv(
+        const_path+r"\title.basics.tsv.gz"
+        , sep='\t'
+        , dtype={'startYear': object}
+        , compression='gzip'
+    )
+    movie_titles = titles_raw[titles_raw['titleType'] == 'movie']
+    movie_titles = pd.merge(movie_titles, kaggle_data[['tconst', 'country']], on='tconst')
+    print("Titles data loaded")
+    return movie_titles
+
+def load_person_data():
+    print("Loading Person Data")
+    person_raw = pd.read_csv(
+        const_path+r"\name.basics.tsv.gz"
+        , sep='\t'
+        , compression='gzip'
+    )
+    print("Person data loaded")
+    return person_raw
 
 
-titles = p.load_title_data()
-genres = p.load_rating_data(titles)
-rating = p.load_rating_data(titles)
+def load_rating_data(movie_titles):
+    print("Loading Rating data")
+    rating_raw = pd.read_csv(
+        const_path + r"\title.ratings.tsv.gz"
+        , sep='\t'
+        , compression='gzip'
+    )
+    rating_df = pd.merge(rating_raw, movie_titles, on='tconst', how='inner')
+    print("Rating data loaded")
+    return rating_df
 
-titles_df = p.clean_movie_data(titles)
-genres_df = p.clean_genre_data(genres)
 
-rating_df = p.clean_rating_data(rating)
-rating_df = rating_df[rating_df['num_votes'] > 3155]
+def load_crew_data(movie_titles):
+    print("Loading Movie Crew data")
+    titles_crew_raw = pd.read_csv(
+        const_path + r"\title.crew.tsv.gz"
+        , sep='\t'
+        , compression='gzip'
+    )
+    movie_crew_raw = pd.merge(titles_crew_raw, movie_titles, on='tconst', how='inner')
+    print("Movie Crew data loaded")
+    return movie_crew_raw
 
-genres_df = pd.merge(genres_df, titles_df, on='title_id', how='inner')
-rating_df = pd.merge(rating_df, genres_df, on='title_id', how='inner')
 
-groupby_genre = rating_df.groupby(['genres', 'year'])
+def load_principals_data(movie_titles):
+    print("Loading Title Principals data")
+    titles_principals_raw = pd.read_csv(
+        const_path + r"\title.principals.tsv.gz"
+        , sep='\t'
+        , compression='gzip'
+    )
+    print("Title Principals data loaded")
+    titles_principals_raw = pd.merge(titles_principals_raw, movie_titles, on='tconst', how='inner')
+    return titles_principals_raw
 
-print(groupby_genre)
 
-# titles_raw[titles_raw['titleType'] == 'movie']
+def clean_movie_data(movie_titles):
+    print("Cleaning movie data")
+    movie_titles_df = movie_titles[['tconst', 'primaryTitle', 'startYear', 'runtimeMinutes', 'country']]
+    movie_titles_df = movie_titles_df.replace(r'\N', 'N/A')
+    movie_titles_df = movie_titles_df.replace(r'NaN', 'N/A')
+    movie_titles_df.columns = ['title_id', 'movie_name', 'year', 'runtime', 'country']
+    movie_titles_df = movie_titles_df.drop_duplicates(subset='title_id')
+    movie_titles_df = movie_titles_df.set_index('title_id')
+    print("Movie data cleaned")
+    return movie_titles_df
 
-print(groupby_genre.describe())
-print(groupby_genre.mean())
+def clean_genre_data(movie_titles):
+    print("Cleaning movie genre data")
+    movie_genres = movie_titles[['tconst', 'genres']]
+    movie_genres.columns = ['title_id', 'genres']
+    movie_genres = movie_genres.replace(r'\N', 'N/A')
+    movie_genres["genres"] = movie_genres["genres"].str.split(",")
+    movie_genres["genres"] = movie_genres.genres.map(lambda x: x[0])
+    movie_genres_df = movie_genres
+    movie_genres_df = movie_genres_df[movie_genres_df['genres'] != 'Adult']
+    movie_genres_df = movie_genres_df[movie_genres_df['genres'] != 'War']
+    movie_genres_df = movie_genres_df[movie_genres_df['genres'] != 'Music']
+    movie_genres_df = movie_genres_df[movie_genres_df['genres'] != 'Musical']
+    movie_genres_df = movie_genres_df[movie_genres_df['genres'] != 'Film-Noir']
+    movie_genres_df = movie_genres_df[movie_genres_df['genres'] != 'Family']
+    movie_genres_df = movie_genres_df.set_index('title_id')
+    # print("Pivoting movie genre data")
+    # movie_genres_df = movie_genres.genres.apply(pd.Series) \
+    #     .merge(movie_genres, left_index=True, right_index=True) \
+    #     .drop(["genres"], axis=1) \
+    #     .melt(id_vars=['title_id'], value_name="genres") \
+    #     .drop("variable", axis=1) \
+    #     .dropna() \
+    #     .set_index('title_id')
+    # print("Movie genre data pivoted")
+    print("Genre data cleaned")
+    return movie_genres_df
+
+
+def clean_rating_data(rating_df):
+    print("Cleaning rating data")
+    rating_df = rating_df.replace(r'\N', 'N/A')
+    rating_df = rating_df[['tconst', 'averageRating', 'numVotes']]
+    rating_df.columns = ['title_id', 'average_rating', 'num_votes']
+    rating_df = rating_df.set_index('title_id')
+    rating_df = rating_df[rating_df['num_votes'] > 3155]
+    # 3155 being the mean of the raw data
+    print("Movie rating cleaned")
+    return rating_df
+
+
+def clean_principals_data(title_principals):
+    print("Cleaning titles principals data")
+    title_principals_df = title_principals[['tconst', 'nconst', 'category', 'job']]
+    title_principals_df = title_principals_df.replace(r'\N', 'N/A')
+    title_principals_df.columns = ['title_id', 'person_id', 'category', 'job']
+    title_principals_df = title_principals_df.set_index('title_id')
+    print("Movie titles principals cleaned")
+    return title_principals_df
+
+
+def clean_person_data(person_raw):
+    print("Cleaning person data")
+    person_df = person_raw.replace(r'\N', 'N/A')
+    person_df = person_df[person_df['deathYear'] == 'N/A']
+    person_data_df = person_df[['nconst', 'primaryName', 'birthYear']]
+    person_data_df.columns = ['person_id', 'name', 'birth_year']
+    person_data_df = person_data_df.set_index('person_id')
+    print("Person data cleaned")
+    return person_data_df
+
+
+def clean_directors_data(movie_crew_raw):
+    print("Cleaning directors data")
+    movie_directors = movie_crew_raw[['tconst', 'directors']]
+    movie_directors = movie_directors.replace(r'\N', 'N/A')
+    movie_directors.columns = ['title_id', 'directors']
+    movie_directors["directors"] = movie_directors["directors"].str.split(",")
+    print("Pivoting directors data")
+    movie_directors_df = movie_directors.directors.apply(pd.Series)\
+        .merge(movie_directors, left_index=True, right_index=True)\
+        .drop(["directors"], axis=1)\
+        .melt(id_vars=['title_id'], value_name="directors")\
+        .drop("variable", axis=1)\
+        .dropna()\
+        .set_index('title_id')
+    print("Directors data pivoted")
+    print("Directors data cleaned")
+    return movie_directors_df
+
+
+def clean_writers_data(movie_crew_raw):
+    print("Cleaning writers data")
+    movie_writers = movie_crew_raw[['tconst', 'writers']]
+    movie_writers = movie_writers.replace(r'\N', 'N/A')
+    movie_writers.columns = ['title_id', 'writers']
+    movie_writers["writers"] = movie_writers["writers"].str.split(",")
+    print("Pivoting Writers data")
+    movie_writers_df = movie_writers.writers.apply(pd.Series)\
+        .merge(movie_writers, left_index=True, right_index=True)\
+        .drop(["writers"], axis=1)\
+        .melt(id_vars=['title_id'], value_name="writers")\
+        .drop("variable", axis=1)\
+        .dropna()\
+        .set_index('title_id')
+    print("Writers data pivoted")
+    print("Writers data cleaned")
+    return movie_writers_df
+
+def load_tables(con, cur, movie_titles_df, movie_genres_df, person_data_df, rating_df,
+                movie_directors_df, movie_writers_df, title_principals_df):
+    print("Creating tables")
+    with con:
+        cur.execute("DROP TABLE IF EXISTS tbl_movie_titles")
+        movie_titles_df.to_sql('tbl_movie_titles'
+                               , con=con
+                               , schema=None
+                               , if_exists='fail'
+                               , index=True
+                               , index_label=None
+                               , chunksize=None
+                               , dtype=None)
+        cur.execute("DROP TABLE IF EXISTS tbl_movie_genres")
+        movie_genres_df.to_sql('tbl_movie_genres'
+                               , con=con
+                               , schema=None
+                               , if_exists='fail'
+                               , index=True
+                               , index_label=None
+                               , chunksize=None
+                               , dtype=None)
+        cur.execute("DROP TABLE IF EXISTS tbl_person_data")
+        person_data_df.to_sql('tbl_person_data'
+                              , con=con
+                              , schema=None
+                              , if_exists='fail'
+                              , index=True
+                              , index_label=None
+                              , chunksize=None
+                              , dtype=None)
+        cur.execute("DROP TABLE IF EXISTS tbl_movie_rating")
+        rating_df.to_sql('tbl_movie_rating'
+                         , con=con
+                         , schema=None
+                         , if_exists='fail'
+                         , index=True
+                         , index_label=None
+                         , chunksize=None
+                         , dtype=None)
+        cur.execute("DROP TABLE IF EXISTS tbl_movie_directors")
+        movie_directors_df.to_sql('tbl_movie_directors'
+                                  , con=con
+                                  , schema=None
+                                  , if_exists='fail'
+                                  , index=True
+                                  , index_label=None
+                                  , chunksize=None
+                                  , dtype=None)
+        cur.execute("DROP TABLE IF EXISTS tbl_movie_writers")
+        movie_writers_df.to_sql('tbl_movie_writers'
+                                , con=con
+                                , schema=None
+                                , if_exists='fail'
+                                , index=True
+                                , index_label=None
+                                , chunksize=None
+                                , dtype=None)
+        cur.execute("DROP TABLE IF EXISTS tbl_movie_principals")
+        title_principals_df.to_sql('tbl_movie_principals'
+                                   , con=con
+                                   , schema=None
+                                   , if_exists='fail'
+                                   , index=True
+                                   , index_label=None
+                                   , chunksize=None
+                                   , dtype=None)
+    print("Tables created, closing connection")
+
+def main():
+    con = lite.connect('imdb_movies_pt2.db')
+    cur = con.cursor()
+    kaggle_data = load_kaggle_data()
+    movie_data = load_title_data(kaggle_data=kaggle_data)
+    person_data = load_person_data()
+    rating_data = load_rating_data(movie_titles=movie_data)
+    crew_data = load_crew_data(movie_titles=movie_data)
+    principals_data = load_principals_data(movie_titles=movie_data)
+    movie_data_df = clean_movie_data(movie_titles=movie_data)
+    movie_genres_df = clean_genre_data(movie_titles=movie_data)
+    person_data_df = clean_person_data(person_raw=person_data)
+    rating_data_df = clean_rating_data(rating_df=rating_data)
+    directors_data_df = clean_directors_data(movie_crew_raw=crew_data)
+    writers_data_df = clean_writers_data(movie_crew_raw=crew_data)
+    principals_data_df = clean_principals_data(title_principals=principals_data)
+    load_tables(con=con,
+                cur=cur,
+                movie_titles_df=movie_data_df,
+                movie_genres_df=movie_genres_df,
+                person_data_df=person_data_df,
+                rating_df=rating_data_df,
+                movie_directors_df=directors_data_df,
+                movie_writers_df=writers_data_df,
+                title_principals_df=principals_data_df)
+    cur.close()
+    con.close()
+
+
+if __name__ == '__main__':
+    main()
